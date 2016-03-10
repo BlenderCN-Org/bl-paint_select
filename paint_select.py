bdd91c66e180074120670c28a9fb77601c932da1
Adam Wolski <miniukof@gmail.com>
2016-03-10 18:55:30 +0100

    Create separate repository for paint select

---
+++ paint_select.py
@@ -0,0 +1,65 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+# Based on paint select by Kjartan
+# http://blenderartists.org/forum/showthread.php?383998
+
+import bpy
+from bpy.props import BoolProperty
+
+
+bl_info = {
+    'name'        : "Paint Selection",
+    'author'      : "miniukof",
+    'description' : "Select by painting on the objects. Modo/Softimage style.",
+    'category'    : "Mesh",
+    'blender'     : (2, 76, 11),
+    'version'     : (0, 0, 2),
+    'wiki_url'    : 'https://github.com/miniukof/bl-paint_select',
+}
+
+class PaintSelect(bpy.types.Operator):
+    bl_idname = "view3d.paint_select"
+    bl_label = "Paint Select"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    deselect = BoolProperty(name="Deselect",
+                            default=False)
+
+    def modal(self, context, event):
+        if self.deselect:
+            bpy.ops.view3d.select('INVOKE_DEFAULT',
+                                  extend=False,
+                                  deselect=True,
+                                  toggle=True)
+        else:
+            bpy.ops.view3d.select('INVOKE_DEFAULT',
+                                  extend=True,
+                                  deselect=False)
+        if event.value == 'RELEASE':
+            return {'FINISHED'}
+        return {'RUNNING_MODAL'}
+
+    def invoke(self, context, event):
+        context.window_manager.modal_handler_add(self)
+        return {'RUNNING_MODAL'}
+
+def register():
+    bpy.utils.register_class(PaintSelect)
+def unregister():
+    bpy.utils.unregister_class(PaintSelect)
