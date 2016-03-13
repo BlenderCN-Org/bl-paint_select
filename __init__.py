0b20c9833ffd15686f56695c95798a78c8590a91
Adam Wolski <miniukof@gmail.com>
2016-03-13 20:08:32 +0100

    Add init file so add-on can work from zip files.

---
+++ __init__.py
@@ -0,0 +1,21 @@
+import bpy
+
+from . import paint_select
+
+
+bl_info = {
+    'name'        : "Paint Selection",
+    'author'      : "miniukof",
+    'description' : "Select by painting on the objects. Modo/Softimage style.",
+    'category'    : "Mesh",
+    'blender'     : (2, 76, 11),
+    'version'     : (0, 0, 2),
+    'wiki_url'    : 'https://github.com/miniukof/bl-paint_select',
+}
+
+
+def register():
+    bpy.utils.register_module(__name__)
+
+def unregister():
+    bpy.utils.unregister_module(__name__)
